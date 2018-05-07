6d6dd6c8d362e3286ee75c3e921211352158c025
alex7alves <alex7alves10@hotmail.com>
2018-05-07 17:18:19 -0300

    Inserindo settings production.py

--- Cadastro/testedjango/projetodjango/projetodjango/urls.py
+++ Cadastro/testedjango/projetodjango/projetodjango/urls.py
@@ -13,9 +13,29 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
+
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    
 ]
+
+"""
+urlpart = patterns('',
+
+
+)
+
+if settings.DEBUG:
+	urlpart += patterns('',(r'^media/(?P<path>.*)$',
+		'django.views.static.serve',{'document_root':settings.MEDIA_ROOT,'show_indexes':True}),
+
+	)
+
+urlpart += staticfiles_urlpatterns	
+"""
