b006b65e463100bc2dbe46cb8a5bcc00f0edc108
alex7alves <alex7alves10@hotmail.com>
2018-05-28 09:42:13 -0300

    Pegando e-mails de um arquivo

--- regex.py
+++ regex.py
@@ -18,6 +18,20 @@ def casamento(regex,string):
 	else:
 		print " Não encontrou-se caracteres" 
 
+
+def separar(regex,string):
+	casou = re.search(regex, string)
+	return casou
+
+def casamentos(regex,string):
+	strings = re.findall(regex,string)
+	return strings
+
+
+
+
+
+
 # o \w indica a quantidade de letras as ser cadasdas junto ao al
 casamento(r'al\w\w','alex lindo')
 casamento(r'al\w\w\w','só 10')
@@ -35,3 +49,24 @@ math = re.compile(r'\d{2}')
 math.match('O  numero 10 foi encontrado 20 vezes')
 if math ==None:
 	print "nao casou"
+
+'''
+ + -> 1 ou mais vezes
+* 0 ou mais
+? 0 ou 1
+'''
+casamento(r'\d+\w?', 'Foram 2047235s gastos')
+
+# Pegar o email da frase
+email =separar(r'([\w . \- _]+)@([\w . \- _]+)', 'o e-mail é joven_25@gmail.com')
+print " o e-mail é ",email.group()
+print "o login é ",email.group(1)
+print "o domínio é ",email.group(2)
+
+
+# Pegando e-mails de um arquivo
+arquivo = open('Exemplo_emails.txt','r')
+strings = casamentos(r'[\w . \- _]+@[\w . \- _]+',arquivo.read())
+print 'Os e-mails do arquivo são :'
+for em in strings:
+	print em 
